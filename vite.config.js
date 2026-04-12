e987e98919e68a01a9d022e0ee7a8ca2c31e12ad
Junaid Ahmed <jahmed.cloud@outlook.com>
2026-04-12 20:45:12 +0000

    Fix asset paths to use relative directories

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // <-- Change this from './' to '/'
+  base: './', // <-- The dot makes the paths relative!
 })
